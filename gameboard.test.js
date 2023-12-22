419378ee4a3b9bdded619bf8121853b72635ea1d
Code25 <chingisenkhbaatar@gmail.com>
2023-12-22 15:08:28 +0100

    rewrote tests to test for correct ship placement,and correct hits received

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,45 +1,32 @@
-require('./gameboard');
+const GameBoard = require('./gameboard.js');
+import './ship.js';
 
-// places the correct number of ships on the game board
-it('should place the correct number of ships on the game board', () => {
-    const numShips = 3;
-    const gameBoard = Array.from({ length: 10 }, () => Array(10).fill(' '));
-
-    placeShips(numShips, gameBoard);
-
-    let shipCount = 0;
-    gameBoard.forEach(row => {
-        row.forEach(cell => {
-            if (cell === 'S') {
-                shipCount++;
-            }
-        });
-    });
-
-    expect(shipCount).toBeGreaterThanOrEqual(numShips);
+// add a ship to the game board
+it('should add a ship to the game board when valid coordinates and length are provided', () => {
+    const gameBoard = new GameBoard();
+    gameBoard.addShip(0, 0, 3, 'horizontal');
+    expect(gameBoard.ships.length).toBe(1);
 });
 
-// places ships in valid positions on the game board
-it('should place ships in valid positions on the game board', () => {
-    const numShips = 3;
-    const gameBoard = Array.from({ length: 10 }, () => Array(10).fill(' '));
+// receive an attack and check if it was a hit
+it('should return true when receiving an attack that hits a ship', () => {
+    const gameBoard = new GameBoard();
+    gameBoard.addShip(0, 0, 3, 'horizontal');
+    const result = gameBoard.receiveAttack(1, 0);
+    expect(result).toBe(true);
+});
 
-    placeShips(numShips, gameBoard);
+// receive an attack and check if it was a miss
+it('should return false when receiving an attack that misses all ships', () => {
+    const gameBoard = new GameBoard();
+    gameBoard.addShip(0, 0, 3, 'horizontal');
+    const result = gameBoard.receiveAttack(3, 0);
+    expect(result).toBe(false);
+});
 
-    let shipCount = 0;
-    for (let i = 0; i < gameBoard.length; i++) {
-        for (let j = 0; j < gameBoard[i].length; j++) {
-            if (gameBoard[i][j] === 'S') {
-                shipCount++;
-                if ((i > 0 && gameBoard[i - 1][j] === 'S') ||
-                    (i < gameBoard.length - 1 && gameBoard[i + 1][j] === 'S') ||
-                    (j > 0 && gameBoard[i][j - 1] === 'S') ||
-                    (j < gameBoard[i].length - 1 && gameBoard[i][j + 1] === 'S')) {
-                    throw new Error(`Invalid placement at ${i}, ${j}`);
-                }
-            }
-        }
-    }
-
-    expect(shipCount).toBe(numShips);
+// receive an attack on an empty game board
+it('should return false when receiving an attack on an empty game board', () => {
+    const gameBoard = new GameBoard();
+    const result = gameBoard.receiveAttack(0, 0);
+    expect(result).toBe(false);
 });
