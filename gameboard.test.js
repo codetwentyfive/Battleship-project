24f5bbd5da879dbd47859c216b93039c3619cfe7
Code25 <chingisenkhbaatar@gmail.com>
2023-12-23 02:41:31 +0100

    test: added tests for gameboard

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,6 +1,37 @@
 const GameBoard = require('./gameboard.js');
 import './ship.js';
 
+
+// Should return an array of length 100 when called on a new instance of GameBoard.
+it('should return an array of length 100 when called on a new instance of GameBoard', () => {
+    const gameBoard = new GameBoard();
+    const result = gameBoard.getAllCoordinates();
+    expect(result).toHaveLength(100);
+});
+
+// Should return an array of objects with x and y properties.
+it('should return an array of objects with x and y properties', () => {
+    const gameBoard = new GameBoard();
+    const result = gameBoard.getAllCoordinates();
+    expect(result).toEqual(expect.arrayContaining([
+        expect.objectContaining({ x: expect.any(Number), y: expect.any(Number) })
+    ]));
+});
+
+
+// Should return an array of objects with x and y properties ranging from 0 to 9.
+it('should return an array of objects with x and y properties ranging from 0 to 9', () => {
+    const gameBoard = new GameBoard();
+    const result = gameBoard.getAllCoordinates();
+    for (const coordinate of result) {
+        expect(coordinate).toEqual(expect.objectContaining({ x: expect.any(Number), y: expect.any(Number) }));
+        expect(coordinate.x).toBeGreaterThanOrEqual(0);
+        expect(coordinate.x).toBeLessThanOrEqual(9);
+        expect(coordinate.y).toBeGreaterThanOrEqual(0);
+        expect(coordinate.y).toBeLessThanOrEqual(9);
+    }
+});
+
 // add a ship to the game board
 it('should add a ship to the game board when valid coordinates and length are provided', () => {
     const gameBoard = new GameBoard();
@@ -30,3 +61,5 @@ it('should return false when receiving an attack on an empty game board', () => 
     const result = gameBoard.receiveAttack(0, 0);
     expect(result).toBe(false);
 });
+
+
